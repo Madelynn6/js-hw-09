264583fb24ddb979a64425313dfb37c3edad7898
Madelynn6 <111072431+Madelynn6@users.noreply.github.com>
2023-01-14 22:45:12 +0000

    Deploying to gh-pages from @ Madelynn6/js-hw-09@82e54002598521d32b01a406bcd63de728775d5f 🚀

---
+++ 01-color-switcher.789e3c5f.js
@@ -0,0 +1,2 @@
+!function(){var t,e=document.querySelector("button[data-start]"),n=document.querySelector("button[data-stop]"),d=document.querySelector("body");n.disabled=!0,e.addEventListener("click",(function(){n.disabled=!1,e.disabled=!0,t=setInterval((function(){d.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16))}),1e3)})),n.addEventListener("click",(function(){e.disabled=!1,n.disabled=!0,clearInterval(t)}))}();
+//# sourceMappingURL=01-color-switcher.789e3c5f.js.map
